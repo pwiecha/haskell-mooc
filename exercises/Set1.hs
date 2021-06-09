8041262e783eefe3f54c431f69fd2baebb012377
Pawel Wiecha <pawel.wiecha@gmail.com>
2021-06-09 15:23:05 +0200

    Finished Set1

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -20,6 +20,10 @@ import Mooc.Todo
 ------------------------------------------------------------------------------
 -- Ex 1: define variables one and two. They should have type Int and
 -- values 1 and 2, respectively.
+one :: Int
+one = 1
+two :: Int
+two = 2
 
 
 ------------------------------------------------------------------------------
@@ -27,7 +31,7 @@ import Mooc.Todo
 -- should take one argument and return it multiplied by two.
 
 double :: Integer -> Integer
-double x = todo
+double x = x * 2
 
 ------------------------------------------------------------------------------
 -- Ex 3: define the function quadruple that uses the function double
@@ -35,7 +39,7 @@ double x = todo
 -- four.
 
 quadruple :: Integer -> Integer
-quadruple x = todo
+quadruple x = double (double x)
 
 ------------------------------------------------------------------------------
 -- Ex 4: define the function distance. It should take four arguments of
@@ -50,8 +54,8 @@ quadruple x = todo
 -- Examples:
 --   distance 0 0 1 1  ==>  1.4142135...
 --   distance 1 1 4 5  ==>  5.0
-
-distance = todo
+distance :: Double -> Double -> Double -> Double -> Double
+distance x1 y1 x2 y2 = sqrt ((x1-x2)^2 + (y1-y2)^2)
 
 ------------------------------------------------------------------------------
 -- Ex 5: define the function eeny that returns "eeny" for even inputs
@@ -60,7 +64,9 @@ distance = todo
 -- Ps. have a look at the built in function "even"
 
 eeny :: Integer -> String
-eeny = todo
+eeny n
+    | even n = "eeny"
+    | otherwise = "meeny"
 
 ------------------------------------------------------------------------------
 -- Ex 6: here's the function checkPassword from the course material.
@@ -68,9 +74,15 @@ eeny = todo
 -- "mellon".
 
 checkPassword :: String -> String
-checkPassword password = if password == "swordfish"
+{-
+checkPassword password = if password == "swordfish" || password == "mellon"
                          then "You're in."
                          else "ACCESS DENIED!"
+-}
+-- with guards
+checkPassword password
+    | password == "swordfish" || password == "mellon" = "You're in."
+    | otherwise = "ACCESS DENIED!"
 
 ------------------------------------------------------------------------------
 -- Ex 7: A postal service prices packages the following way.
@@ -82,7 +94,10 @@ checkPassword password = if password == "swordfish"
 -- in grams, and returns the cost in credits.
 
 postagePrice :: Int -> Int
-postagePrice = todo
+postagePrice w
+    | w <= 500 = 250
+    | w > 5000 = 6000
+    | otherwise = 300 + w
 
 ------------------------------------------------------------------------------
 -- Ex 8: define a function isZero that returns True if it is given an
@@ -91,8 +106,8 @@ postagePrice = todo
 -- Use pattern matching! Don't use comparisons!
 --
 -- Ps. remember, the type of booleans in haskell is Bool
-
-isZero = todo
+isZero :: Integer -> Bool
+isZero n = if n == 0 then True else False 
 
 ------------------------------------------------------------------------------
 -- Ex 9: implement using recursion a function sumTo such that
@@ -100,14 +115,20 @@ isZero = todo
 -- computes the sum 1+2+...+n
 
 sumTo :: Integer -> Integer
-sumTo = todo
+sumTo 1 = 1
+sumTo n = n + sumTo (n-1)
 
 ------------------------------------------------------------------------------
 -- Ex 10: power n k should compute n to the power k (i.e. n^k)
 -- Use recursion.
 
 power :: Integer -> Integer -> Integer
-power = todo
+power _ 0 = 1
+power n k = n * power n (k-1)
+{- note: power n k-1 in recursive call produces infinite loop
+   because function call takes precedence over subtraction
+   and this evaluates to (power n k) - 1
+-}
 
 ------------------------------------------------------------------------------
 -- Ex 11: ilog3 n should be the number of times you can divide given
@@ -126,4 +147,6 @@ power = todo
 --   ilog3 7 ==> 2
 
 ilog3 :: Integer -> Integer
-ilog3 = todo
+ilog3 n 
+    | n < 3 = 1
+    | otherwise = 1 + ilog3 (n `div` 3)
--- exercises/scratch/ch1.hs
+++ exercises/scratch/ch1.hs
@@ -1,3 +1,5 @@
+module Ch1 where
+
 polynomial :: Double -> Double 
 polynomial x = x^2 - x - 1
 
@@ -14,3 +16,19 @@ circleCircum :: Double -> Double
 circleCircum r = 2 * pi * r
     where pi = 3.1415
 
+
+fact :: Int -> Int
+fact 0 = 1
+fact n = n * fact (n-1)
+
+fact2 :: Int -> Int
+fact2 n = if n == 0 then 1 else n * fact2 (n-1)
+
+-- with guard
+fact3 :: Int -> Int
+fact3 n
+    | n > 1 = n * fact3 (n-1)
+    | otherwise = 1
+
+addd :: Double -> Double -> Double
+addd x y = sq_rt x + sq_rt y
